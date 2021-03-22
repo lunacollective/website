0291c14b29b149e8a42c145edcbf3df755f4719f
lunaisnotaboy <her@mint.lgbt>
2021-03-22 18:10:05 -0400

    fix: wrong tailwind class

--- components/Title.vue
+++ components/Title.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="title">
     <h1
-      class="sd:text-6xl font-bold font-sans text-2xl text-gray-900 dark:text-white"
+      class="sm:text-6xl font-bold font-sans text-2xl text-gray-900 dark:text-white"
     >
       The <span class="text-purple-700">Luna</span> Collective
     </h1>
